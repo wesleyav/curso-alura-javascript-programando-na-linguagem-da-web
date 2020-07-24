cd9dec65936704277acd6f0488f97bd4502c0c11
Wesley <wesley.contato@hotmail.com>
2020-07-24 08:03:50 -0300

    Melhorando ainda mais o código

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -11,30 +11,8 @@ botaoAdicionar.addEventListener('click', function (event) {
     // Instanciando o objeto
     var paciente = obtemPacienteDoFormulario(form);
 
-
     // Criando a nova linha <tr>
-    var pacienteTr = document.createElement("tr");
-
-    // Criando as novas colunas <td>
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    // Preenchendo os <td> com os valores que foram digitados no no form
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
-
-    // Adicionando as colunas <td> na linha <tr>
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    var pacienteTr = montaTr(paciente);
 
     // Adicionando os elementos criados no <tbody>
     var tabela = document.querySelector("#tabela-pacientes");
@@ -42,9 +20,12 @@ botaoAdicionar.addEventListener('click', function (event) {
     // Adicionando o <tr> na tabela que já existia
     tabela.appendChild(pacienteTr);
 
+    // Limpa os dados do form após clica no botão adicionar
+    form.reset();
+
 });
 
-function obtemPacienteDoFormulario(form){
+function obtemPacienteDoFormulario(form) {
 
     var paciente = {
         nome: form.nome.value,
@@ -55,4 +36,27 @@ function obtemPacienteDoFormulario(form){
     }
 
     return paciente;
+}
+
+function montaTr(paciente) {
+
+    var pacienteTr = document.createElement("tr"); // cria a TR
+    pacienteTr.classList.add("paciente"); // adiciona a classe paciente ao HTML
+
+    // Adicionando as colunas <td> na linha <tr> já com suas classes
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
 }
