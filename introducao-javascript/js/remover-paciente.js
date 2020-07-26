b1176935612fcf82bd664414edebbb450a7270f6
Wesley <wesley.contato@hotmail.com>
2020-07-26 11:15:43 -0300

    Filtrando pacientes da tabela

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -2,9 +2,10 @@ var pacientes = document.querySelectorAll(".paciente");
 
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(event){
-    var alvoEvento =event.target;
-    var paiDoAlvo = alvoEvento.parentNode; // TR = paciente a remover
-    paiDoAlvo.remove();
-    // event.target.parentNode.remove(); // Tudo em apenas uma linha
+tabela.addEventListener("dblclick", function (event) {
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function () {
+        event.target.parentNode.remove(); // Tudo em apenas uma linha
+    }, 500);
 });
